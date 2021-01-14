f96d3b1b66b07c1849e26091c4c5168137f380b0
maxyodedara5 <maxyodedara5@gmail.com>
2021-01-14 22:20:20 +0530

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,76 +1,18 @@
 <?php
-if (isset($_POST['Email'])) {
+if(!empty($_POST["send"])) {
+	$name = $_POST["contactName"];
+	$email = $_POST["contactEmail"];
+	$subject = $_POST["contactSubject"];
+	$content = $_POST["contactMessage"];
 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "maxyodedara5@gmail.com";
-    $email_subject = "TrailError";
-
-    function problem($error)
-    {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
-        echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
-        die();
-    }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['contactName']) ||
-        !isset($_POST['contactEmail']) ||
-        !isset($_POST['contactMessage'])
-    ) {
-        problem('We are sorry, but there appears to be a problem with the form you submitted.');
-    }
-
-    $name = trim(stripslashes($_POST['contactName'])); // required
-    $email = trim(stripslashes($_POST['contactEmail'])); // required
-    $message = trim(stripslashes($_POST['contactMessage'])); // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The Name you entered does not appear to be valid.<br>';
-    }
-
-    if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
-    }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details below.\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
-
-    // create email headers
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
-?>
-
-    <!-- include your success message below -->
-
-    Thank you for contacting us. We will be in touch with you very soon.
-
-<?php
+	$toEmail = 'maxyodedara5@gmail.com';
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}else{
+		echo "Something went wrong. Please try again.";
+	}
 }
+require_once "contact-view.php";
 ?>
