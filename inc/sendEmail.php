e7532b94d4a85be1b7939af7ce96ed8678fd5024
maxyodedara5 <maxyodedara5@gmail.com>
2021-01-14 22:28:02 +0530

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,18 +1,38 @@
-<?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["contactName"];
-	$email = $_POST["contactEmail"];
-	$subject = $_POST["contactSubject"];
-	$content = $_POST["contactMessage"];
-
-	$toEmail = 'maxyodedara5@gmail.com';
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}else{
-		echo "Something went wrong. Please try again.";
-	}
+<?php 
+$errors = '';
+$myemail = 'maxyodedara5@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['contactName'])  || 
+   empty($_POST['contactEmail']) || 
+   empty($_POST['contactMessage']))
+{
+    $errors .= "\n Error: all fields are required";
 }
-require_once "contact-view.php";
+
+$name = $_POST['contactName']; 
+$email_address = $_POST['contactEmail']; 
+$message = $_POST['contactMessage']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	$mail = mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+
+	if ($mail) { echo "OK"; }
+	else { echo "Something went wrong"; }
+} 
 ?>
