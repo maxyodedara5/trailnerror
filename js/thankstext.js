8539f3eef39ea6c568bbd070e94a79d2557bf8e8
maxyodedara5 <maxyodedara5@gmail.com>
2021-01-16 13:04:49 +0530

    UI changes - Color scheme blue
    Color scheme blue

---
+++ js/thankstext.js
@@ -0,0 +1,7 @@
+function FormSubmit() { 
+		
+			
+    document.getElementById('texto').innerHTML = "Thanks for reaching out, if form is submitted, I'll get back to you as soon as possible";
+    
+
+}
